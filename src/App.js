aef617a0f6787d4976d242077dbdf9675e7be01b
singleshapar <arshadumar71@gmail.com>
2022-01-01 04:45:57 +0500

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import { Routes, Route } from 'react-router-dom';
+import AddApplicant from './components/AddApplicant';
+import ViewApplicant from './components/ViewApplicant';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-center">Hostel Application System</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/hostelApplicants" element={<AddApplicant />} />
+        <Route
+          path="/viewApplicants"
+          element={<ViewApplicant />}
+        />
+      </Routes>
     </div>
   );
 }
